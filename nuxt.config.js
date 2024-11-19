37bd47cc8bfb4943dca691d9e86da529aeec8174
Marcos José Smarzaro Gramelich <gramelich@medware.onmicrosoft.com>
2024-11-19 09:38:46 -0300

    Add config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,17 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt App",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+
   ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
